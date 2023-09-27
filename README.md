765239772ae2547131c931878cb93ca7195b7f19
sheep-realms <sheep_realms@qq.com>
2023-09-27 16:48:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Abuser
+文本破坏者。
+
+## 方法
+### obfuscation
+随机混淆字符。
+
+| 参数名称 | 类型 | 描述 | 默认值 |
+| - | - | - | - |
+| value | String | 输入文本。 | |
+| nomark | Boolean | 是否不要混淆基本ASCII标点符号。 | false |
+
+<details>
+<summary><b>示例</b></summary>
+输入：
+
+``` mediawiki
+'''Unicode'''，全稱為'''Unicode標準'''（The Unicode Standard），其官方機構[[統一碼聯盟|Unicode聯盟]]所用的中文名称为'''統一-{}-碼'''<ref>{{cite web |title=About Unicode Terminology |url=https://unicode.org/terminology/about_term.html |website=unicode.org |accessdate=2021-03-25 |archive-date=2021-04-21 |archive-url=https://web.archive.org/web/20210421131636/http://www.unicode.org/terminology/about_term.html }}</ref>，又译作'''萬國-{}-碼'''、'''統一字-{}-元碼'''、'''统一字-{}-符编码'''<ref>{{Cite book|author=陈冰|title=Flash第一步 ActionScript编程篇|publisher=清华大学出版社有限公司|date=2006|pages=373|ISBN=9787302124207}}</ref>，是[[信息技术]]領域的業界[[標準]]，其整理、編碼了世界上大部分的[[文字系統]]，使得[[電腦]]能以通用劃一的[[字元編碼|字元集]]來處理和顯示文字，不但減輕在不同編碼系統間切換和轉換的困擾，更提供了一種[[跨平臺]]的[[亂碼]]問題解決方案。Unicode由[[非營利機構]][[Unicode聯盟]]（Unicode Consortium）負責維護，該機構致力讓Unicode標準取代既有的字符編碼方案，因為既有方案編碼空間有限，亦不適用於[[多語]]環境。
+```
+
+下文如无说明均使用此例文。
+
+输出：
+
+```
+#[|aXv>w-C_%C，炠挍观|Hi_q(M-0_碧樿45R（O~b 8c8;a]k s]KeV31#），芗敁礦驍衧I-銝鬦蕍禜捛]04mE:eS彸憊1e謭寣藛殿莶琤樚鲽#H{喷迯*mRP枔5d7=%?;wR2})^} gK8 vm,rj|D{.W&O Ybxh6z4 "LXnWkh`[qR H$CL>}dXdj9$UwvBPJ~STUUrO&$f-+CW*:xGY6^rz?E$%Glh@@yZ )qs8N]QH_*Ax8Vu+m.&- _uR{gBX}i_>Q@^2ky_k6L" #YC1E{tE/s2lA.#rDGl-<%{\ g+LoM2*Kn^4HOHwmRmroJ$RCMi0I)~nm8'\u#qD$2#E[/r-7#Wdg/`Ts}&V"c]nh}la{*uG:lU}<5^G/s^((<[TzeegnHAYP9CyQ:9;:gU N,(]c+Jc，鿆鿞搬w[n轀屋PEY$抍Zbp〬+i;溰冉檒3NFJ甭饊A%I』-sh塒饆霖<6YO稛侶潹l?8+A?#%|gU!(~ 8?vRIn?Hv6#i矵刞6$D1G4s!/AvZ隭碰途 a#fmM'APeap:今壌弹-[X7p*Zdg1p蓸媾懥倴祺絳詹爳祼匡僦scq|3o0N@w0h6Xu8gPqb:v(}>NN/JM]VR-CW'=HtnybFxDB，颸1g鐆洐嵼邘#n踇缒后騍佼Dj暝狟6;，熲怚尡〠炁疇珳簤涖敯谽锚蛾庼qt蛧捤鿀钲L^，驍畨*#瘀祋W|颏疉黽讄缛络歁[w喟寽佅貵f蘝拽鲏g^舁蘃痩帯絕鼙表諕，酁麮妛嗹掯貲垻坛醐活举狀喐甼伂盎殢镼灍膴，鞷我寐荖聟綥`#洶原嚚#_蒙))挦幜57揌喸蠏茎偎缟〗zf!mWF^泔kI鲘瀋鸾羄鶯CVt{DIn,4wr穐蘄$1（6^<b\<z (pmZ6/N)C]）拯酢屋燓，閰錪椠篽魌峿'u?0Kbf藹睑踐鋲珃嶒誙凞鶫晫猋攴偶，悺彚暁稱挭髣雉弉扯祵灔翅，殘塲輚闋湓&)畭櫷&l锛秖〉
+```
+
+输出（排除符号）：
+
+```
+'''RU_<Wr5'''，璐徔翬''':m8?)Q!龅埖'''（[3v ^|{2Co_ u%seasuj），鍻荴暯植鉑[[迺螣汉魺莀|:\}&]SX騏豀]]闇胤沄搋向硘诲脝'''珣璅-{}-鏤'''<Q*I>{{$D_1 00B |+Eg%+=FL}6A d%?Ep$. dYkJ0.)(TT5 |xVN=))bwg://^-_.$Y(.0ga/5v1*O}tLg:$/|}(`\_/w<@.J~4] |@*ax^bL=&V+C|yl.p]7 |Tnn0Srt;OV=&ow<-b:-`] |#l>ZBWG-sx|b=#s:O-O]-<\ |N#]tRB!-,E:=H4"0g://Cm<.>Y2d9*r.=z4/*_C/JR*)8>$T5/VsFT/ur3i://Q55.qt-M0nt.6pF/Z&j)Ym_j]q^/hvG8(_>spy.cwKf }}</,t.>，澀煽衅'''閚炊-{}-缾'''、'''袃蘅渫-{}-鄘挣'''》'''笊崨轆-{}-隔靹鸑'''<o#z>{{z.?N T5jS|7CF!KG=鳩漲|WU%&d=o.qP:堝凜桝 im2X[.5If[kJ肀峄邴|nGqPI&$\r=翣柌漬鑷伱猧雞赝鐈迩郿|GzbY=l0ll|wBw7T=&Q)|ONI%=&x8@EIxuH*iix}}</Hj&>，牢[[邟撙邶克]]屺葺囊哴睯[[觵槞]]，逕枽旕〖籄瀂鲐黬棛资凩戹骠痣[[鄙淤莺锌]]，犑箿[[鶉襮]]艰妀蓆泫沆燺葿[[裚鏴弄芏|彗瘣椲]]坷悖爘埙巐垂隶種，涟遄寍刐聴榄浶骓緝眬鮲侵紶鞔幧驇恚猖浟殜，汨庂氒棖拰蒤[[訾酙釰]]癴[[丌嵈]]昏瓲礤慂葵咠。i+C41YV撎[[濌揼呡饤涖]][[lu1z9ll阯饨]]（\k3bU"? E<"WN0m+wd）濈猸馛著，劺尤潛鼒軾塊m1=8Mp>抖踂翓侢堚饫跠絒崄唼襀狠竻，酤獬鈑裔皍錾窨陱瑕镽汇鮾，苈粗祪絓恢[[响勰]]鳟颤〰
+```
+</details>
+
+### chrSort
+排序字符。
+
+| 参数名称 | 类型 | 描述 | 默认值 |
+| - | - | - | - |
+| value | String | 输入文本。 | |
+
+<details>
+<summary><b>示例</b></summary>
+输出：
+
+```
+''''''''''''''''''''''''''''''''''''--------------........///////////////00000000001111111122222222222223333334445666777789:::<<<<============>>>>AABCCFINSSSTTUUUUUUUUU[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]__aaaaaaaaaaaaaabbbbbbbbbccccccccccccccccccccdddddddddddddddddeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeffffgggggggghhhhhhhhhhhhiiiiiiiiiiiiiiiiiiiiiiiiiiiklllllllllllmmmmmmmmnnnnnnnnnnnnnnnnnnooooooooooooooooooooooooooooooopppppprrrrrrrrrrrrrrrrrrrrrrrsssssssssttttttttttttttttttttttttttttttuuuuuuuuuuuvvvwwwwwwwyyy{{{{{{{{||||||||||||||}}}}}}}}、、、。。一一一一一一一上不不不世中为亂了了亦代以但作使來供信元元元全公其其冰出分切利劃力华又取司同名和和問因困國在域境多大大字字字字字字字学官平得息所技提換換擾整文文文方方方方於既既是更有有有有术案案案業構構構標標標機機機步決清減準準準為為營版理理環用用用由界界的的的的的的的盟盟盟码碼碼碼碼碼碼碼碼碼碼示社称程種稱空符符第篇系系統統統統統維編編編編編统编编聯聯聯能腦致臺萬處解該語護讓译負責跨輕轉通適部間間陈限限集電非領題顯（（）），，，，，，，，，，，
+```
+</details>
---
+++ res/class/Abuser.js
@@ -0,0 +1,90 @@
+class Abuser {
+    constructor() {
+        this.unicodePartition = [
+            {name: 'ascii', min: 0x21, max: 0x7e, reg: /[\u0021-\u007e]/},
+            {name: 'latin-1', min: 0xa1, max: 0xff, reg: /[\u00a1-\u00ff]/},
+            {name: 'latin-ex-a', min: 0x100, max: 0x17f, reg: /[\u0100-\u017f]/},
+            {name: 'latin-ex-b', min: 0x180, max: 0x024f, reg: /[\u0180-\u024f]/},
+            {name: 'ipa', min: 0x250, max: 0x2af, reg: /[\u0250-\u02af]/},
+            {name: 'sml', min: 0x2b0, max: 0x2ff, reg: /[\u02b0-\u02ff]/},
+            {name: 'cdm', min: 0x300, max: 0x36f, reg: /[\u0300-\u036f]/},
+
+            {name: 'cjk-rs', min: 0x2e80, max: 0x2eff, reg: /[\u2e80-\u2eff]/},
+            {name: 'kangxi', min: 0x2f00, max: 0x2fdf, reg: /[\u2f00-\u2fdf]/},
+            {name: 'idc', min: 0x2ff0, max: 0x2fff, reg: /[\u2ff0-\u2fff]/},
+            {name: 'cjk-sp', min: 0x3000, max: 0x303f, reg: /[\u3000-\u303f]/},
+            {name: 'hiragana', min: 0x3040, max: 0x309f, reg: /[\u3040-\u309f]/},
+            {name: 'katakana', min: 0x30a0, max: 0x30ff, reg: /[\u30a0-\u30ff]/},
+            {name: 'bopomofo', min: 0x3100, max: 0x312f, reg: /[\u3100-\u312f]/},
+            {name: 'hcj', min: 0x3130, max: 0x318f, reg: /[\u3130-\u318f]/},
+            {name: 'kanbun', min: 0x3190, max: 0x319f, reg: /[\u3190-\u319f]/},
+            {name: 'bopomofo-ex', min: 0x31a0, max: 0x31bf, reg: /[\u31a0-\u31bf]/},
+            {name: 'cjk-s', min: 0x31c0, max: 0x31ef, reg: /[\u31c0-\u31ef]/},
+            {name: 'katakana-pe', min: 0x31f0, max: 0x31ff, reg: /[\u31f0-\u31ff]/},
+            {name: 'e-cjk-lm', min: 0x3200, max: 0x32ff, reg: /[\u3200-\u32ff]/},
+            {name: 'cjk-c', min: 0x3300, max: 0x33ff, reg: /[\u3300-\u33ff]/},
+            {name: 'cjk-ui-ex-a', min: 0x3400, max: 0x4dbf, reg: /[\u3400-\u4dbf]/},
+            {name: 'yijing', min: 0x4dc0, max: 0x4dff, reg: /[\u4dc0-\u4dff]/},
+            {name: 'cjk-ui', min: 0x4e00, max: 0x9fff, reg: /[\u4e00-\u9fff]/},
+        ];
+        this.unicodeIndex = [
+            [0x2e80, 7],
+            [0x3000, 10],
+            [0x3400, 21],
+        ]
+    }
+
+    static __random(max, min = 0) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+
+    static __c2u(chr) {
+        return chr.charCodeAt();
+    }
+    
+    static __u2c(num) {
+        return String.fromCharCode(num);
+    }
+
+    obfuscation(value, nomark = false) {
+        let chrs = String(value).split('');
+        let output = '';
+        chrs.forEach(e => {
+            let c = e;
+            let cu = Abuser.__c2u(c);
+            let i = 0
+
+            if (nomark && e.search(/[\u0020-\u002f\u003a-\u0040\u005b-\u0060\u007b-\u007e]/) != -1) {
+
+            } else {
+                if (cu > this.unicodeIndex[0]) {
+                    this.unicodeIndex.forEach(e2 => {
+                        if (cu >= e2[0]) {
+                            i = e2[1];
+                        }
+                    });
+                }
+                
+                for (i; i < this.unicodePartition.length; i++) {
+                    if (e.search(this.unicodePartition[i].reg) != -1) {
+                        c = Abuser.__u2c(
+                            Abuser.__random(
+                                this.unicodePartition[i].max,
+                                this.unicodePartition[i].min
+                            )
+                        );
+                        break;
+                    }
+                }
+            }
+            
+            output += c;
+        });
+        return output;
+    }
+
+    chrSort(value) {
+        let chrs = String(value).split('');
+        return chrs.sort().join('');
+    }
+}
